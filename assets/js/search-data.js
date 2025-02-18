70e743db61f86f61f7c4cc3f0599bdec9c27769d
leopt4 <135542514+leopt4@users.noreply.github.com>
2025-02-18 03:11:34 +0000

    Deploying to gh-pages from @ leopt4/leopt4.github.io@f2bf624c2ae07fcd9c0109fe15ab191068380256 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -395,9 +395,9 @@ ninja.data = [{
           section: "News",},{id: "news-i-particpated-in-drone-days-4th-aerial-vehicles-end-user-workshop-in-zagreb-croatia",
           title: 'I particpated in Drone Days - 4th Aerial Vehicles end-user workshop in Zagreb,...',
           description: "",
-          section: "News",},{id: "projects-task-priority-kinematic-control-system-for-a-mobile-manipulator",
-          title: 'Task-Priority Kinematic Control System for a Mobile Manipulator',
-          description: "Hands-On Intervention",
+          section: "News",},{id: "projects-task-priority-kinematic-control-for-a-mobile-manipulator",
+          title: 'Task-Priority Kinematic Control for a Mobile Manipulator',
+          description: "Intervention, Pick and Place task",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-formation-control-of-multi-robot-systems-using-consensus-protocols",
@@ -415,9 +415,9 @@ ninja.data = [{
           description: "Localization",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-autonomous-exploration-in-3d-environment-for-turtlebot",
+          title: 'Autonomous Exploration in 3D environment for Turtlebot',
+          description: "Planning, TARE global planner, Falco local planner",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
